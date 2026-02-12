0d6c8a4970cbfd563db4106df836d096072be22f
Berty Liu <bertylliu@gmail.com>
2026-02-13 02:53:41 +0800

    delete main_game scene and change all collison shape to circle or capsule

--- scripts/damage_number.gd
+++ scripts/damage_number.gd
@@ -19,7 +19,7 @@ func display_number(value: int, position: Vector2, is_critical: bool = false):
 	number.label_settings.font_color = color
 	number.label_settings.font_size = 32
 	number.label_settings.outline_color = '#000'
-	number.label_settings.outline_size = 1
+	number.label_settings.outline_size = 4
 	
 	call_deferred("add_child", number)
 	
