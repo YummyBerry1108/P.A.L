a42d01365bf68603404381b7a75f42199a5385b9
Anemois <danieltang1668@gmail.com>
2026-02-13 21:02:32 +0800

    Multishot and multiple projectiles at once done.

--- scripts/player/attack.gd
+++ scripts/player/attack.gd
@@ -16,25 +16,30 @@ func cooldown(player, skills):
 		if not cooldowns.has(skill_name) or now_time >= cooldowns[skill_name]:
 			var new_projectile: Projectile = skill_node.instantiate()
 			cooldowns[skill_name] = now_time + (1 / new_projectile.firerate)
-			how_to_shoot(player, new_projectile, skill_data)
+			how_to_shoot(player, new_projectile, skill_data, skill_node)
 
-func how_to_shoot(player: CharacterBody2D, projectile: Projectile, skill_data: SkillData):
+func how_to_shoot(player: CharacterBody2D, projectile: Projectile, skill_data: SkillData, skill_node):
 	if skill_data.attack_type == "Single":
 		var rotations = calculate_directions(player.global_position, player.get_global_mouse_position(), skill_data.projectile_count, min(projectile.arc + projectile.arc_increment * skill_data.projectile_count, 360))
+		#print(rotations)
 		for rot in rotations:
+			#print("ROT : ", rot)
 			var shooting_helper: ShootingHelper = shooting_helper_scene.instantiate()
 			add_child(shooting_helper)
-			shooting_helper.set_shoot_timer(0.1, skill_data.multishot, player, projectile, rot)
+			shooting_helper.set_shoot_timer(0.1, skill_data.multishot, player, projectile, rot, skill_node)
 
 func calculate_directions(base_position: Vector2, target: Vector2, projectile_count, shooting_arc) -> Array:
 	var rot = base_position.direction_to(target).angle()
 	var rotations = []
-	for i in projectile_count:
-		var arc_rad = deg_to_rad(shooting_arc)
-		var increment = arc_rad / (projectile_count - 1)
-		rotations.append(
-			rot +
-			increment * i -
-			arc_rad / 2
-		)
+	if projectile_count > 1:
+		for i in projectile_count:
+			var arc_rad = deg_to_rad(shooting_arc)
+			var increment = arc_rad / (projectile_count - 1)
+			rotations.append(
+				rot +
+				increment * i -
+				arc_rad / 2
+			)
+	else:
+		rotations.append(rot)
 	return rotations
--- scripts/player/shooting_helper.gd
+++ scripts/player/shooting_helper.gd
@@ -1,22 +1,12 @@
 class_name ShootingHelper extends Node
 
-var repeat = 0
-
-func set_shoot_timer(wait, repeats, player: CharacterBody2D, projectile: Projectile, rot: float) -> void:
-	repeat = repeats
-	var timer: Timer = Timer.new()
-	add_child(timer)
-	timer.wait_time = wait
-	timer.timeout.connect(func():
-		if repeat == 0:
-			timer.stop()
-			queue_free()
-			return
-			
-		projectile.position = player.position
-		projectile.rotation = rot
-		#projectile.look_at(player.get_global_mouse_position())
-		player.get_node("Projectiles").add_child(projectile)
-	)
-	
-	timer.start()
+func set_shoot_timer(wait, repeats, player: CharacterBody2D, projectile, rot: float, skill_node) -> void:
+	projectile.queue_free()
+	for i in repeats:
+		var new_projectile: Projectile = skill_node.instantiate()
+		new_projectile.global_position = player.global_position
+		new_projectile.global_rotation = rot
+		#print(new_projectile.global_position, " ", new_projectile.global_rotation, " ", new_projectile.name)
+		player.get_node("Projectiles").add_child(new_projectile)
+		await get_tree().create_timer(0.1).timeout
+	queue_free()
