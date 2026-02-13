f517a495c88f934a08af28f17aad4cfef4ca6057
Berty Liu <bertylliu@gmail.com>
2026-02-13 18:23:20 +0800

    better damage number VFX

--- scripts/damage_number.gd
+++ scripts/damage_number.gd
@@ -28,11 +28,19 @@ func display_number(value: int, position: Vector2, is_critical: bool = false):
 	await number.resized
 	number.pivot_offset = Vector2(number.size / 2)
 	
-	
+	var rng = RandomNumberGenerator.new()
+	var delta_x: float = rng.randf_range(-50.0, 50.0)
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
+	
+	tween.tween_property(
+		number, "scale", Vector2(2, 2), 0.25
+	).set_ease(Tween.EASE_OUT)
 	tween.tween_property(
 		number, "position:y", number.position.y - 24, 0.25
+	).set_ease(Tween.EASE_OUT)
+	tween.tween_property(
+		number, "position:x", number.position.x + delta_x, 0.5
 	).set_ease(Tween.EASE_OUT)
 	tween.tween_property(
 		number, "position:y", number.position.y, 0.5
