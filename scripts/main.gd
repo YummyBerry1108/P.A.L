78a3b835c8c3eb5565ca1fffb039e060e423a532
Berty Liu <bertylliu@gmail.com>
2026-02-17 03:53:01 +0800

    enemy now spawn in enemy container, and can be sync by another spawner

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,9 @@ extends Node2D
 @onready var health_bar: ProgressBar = $UI/HealthBar
 @onready var time_label: Label = $UI/TimeLabel
 @onready var player_container: Node2D = $PlayerContainer
-@onready var spawner: MultiplayerSpawner = $MultiplayerSpawner
+@onready var enemy_container: Node2D = $EnemyContainer
+@onready var player_spawner: MultiplayerSpawner = $PlayerSpawner
+@onready var enemy_spawner: MultiplayerSpawner = $EnemySpawner
 
 var player_scene = preload("res://scenes/player/player.tscn")
 var time_elapsed: float = 0.0
@@ -11,21 +13,26 @@ var is_timer_running: bool = true
 
 func _ready() -> void:
 	Lobby.player_loaded.rpc_id(1)
-	spawner.spawned.connect(_on_spawned)
+	Lobby.server_disconnected.connect(_game_over)
+	player_spawner.spawned.connect(_on_player_spawned)
+	enemy_spawner.spawned.connect(_on_enemy_spawned)
 
 func _process(delta: float) -> void:
 	if is_timer_running:
 		time_elapsed += delta
 		_update_timer_ui()
 
-func _on_spawned(node: Player) -> void:
+func _on_player_spawned(node: Player) -> void:
 	#print("ID: ",  multiplayer.get_unique_id())
 	#print("Spawn: ", node.name)
 	if node.is_multiplayer_authority():
 		node.health_changed.connect(health_bar._set_health)
 		health_bar.init_health(node.hp)
 
-func _on_player_connected(id) -> void:
+func _on_enemy_spawned(node: Enemy) -> void:
+	pass
+
+func _add_player_node(id) -> void:
 	var player: Player = player_scene.instantiate()
 	player.global_position = Vector2.ZERO
 	player.name = str(id)
@@ -33,8 +40,7 @@ func _on_player_connected(id) -> void:
 	player_container.add_child(player)
 	
 	if id == 1:
-		player.health_changed.connect(health_bar._set_health)
-		health_bar.init_health(player.hp)
+		_on_player_spawned(player)
 
 func _on_player_disconnected(id) -> void:
 	if player_container.has_node(str(id)):
@@ -48,14 +54,25 @@ func _update_timer_ui() -> void:
 	var seconds = int(time_elapsed) % 60
 	
 	time_label.text = "%02d:%02d" % [minutes, seconds]
+	
+func _game_over() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+func spawn_enemy(enemy_name: String = "rock", amount: int = 1) -> void:
+	for i in range(amount):
+		var enemy_node: PackedScene = load("res://scenes/enemies/" + enemy_name + ".tscn")
+		var new_enemy: Enemy = enemy_node.instantiate()
+		new_enemy.global_position = Vector2(randf()*2 - 1, randf()*2 - 1) * 500
+		enemy_container.add_child(new_enemy, true)
 
-func spawn_enemy() -> void:
-	var enemy_node: PackedScene = load("res://scenes/enemies/" + "rock" + ".tscn")
-	var new_enemy: Enemy = enemy_node.instantiate()
-	new_enemy.global_position = Vector2(randf(), randf()) * 100
-	add_child(new_enemy)
-
+# Called only on the server.
 func start_game():
 	print("Game Start!")
 	for player_id in Lobby.players:
-		_on_player_connected(player_id) 
+		_add_player_node(player_id) 
+	
+	spawn_enemy("rabbit", 1)
+	spawn_enemy("rock", 1)
+	spawn_enemy("snail", 1)
+	
+
+	
