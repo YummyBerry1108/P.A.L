8bbae6511d0c87911fd1b14a5073d91835022d67
Berty Liu <bertylliu@gmail.com>
2026-02-17 02:49:35 +0800

    make a lobby scene, it can handle player join and leave before start game

---
+++ scripts/lobby_scene.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var player_list: Label = $PlayerList
+
+func _ready() -> void:
+	Lobby.player_connected.connect(player_list._on_lobby_player_connected)
+	Lobby.player_disconnected.connect(player_list._on_lobby_plater_disconnected)
+	Lobby.server_disconnected.connect(_leave_lobby)
+	
+	if Lobby.is_server:
+		Lobby.create_game()
+	else:
+		Lobby.join_game()
+		
+func _start_game() -> void:
+	if not multiplayer.is_server():
+		return
+	Lobby.load_game.rpc("res://scenes/main.tscn")
+
+func _leave_lobby() -> void:
+	Lobby.remove_multiplayer_peer()
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/main.gd
+++ scripts/main.gd
@@ -10,16 +10,8 @@ var time_elapsed: float = 0.0
 var is_timer_running: bool = true
 
 func _ready() -> void:
-	for i in range(100):
-		#spawn_enemy()
-		pass
+	Lobby.player_loaded.rpc_id(1)
 	spawner.spawned.connect(_on_spawned)
-	
-	if multiplayer.is_server():
-		multiplayer.peer_connected.connect(_on_player_connected)
-		multiplayer.peer_disconnected.connect(_on_player_disconnected)
-		
-		_on_player_connected(1)
 
 func _process(delta: float) -> void:
 	if is_timer_running:
@@ -27,8 +19,8 @@ func _process(delta: float) -> void:
 		_update_timer_ui()
 
 func _on_spawned(node: Player) -> void:
-	print("Author: ",  multiplayer.get_unique_id())
-	print("Spawn: ", node)
+	#print("ID: ",  multiplayer.get_unique_id())
+	#print("Spawn: ", node.name)
 	if node.is_multiplayer_authority():
 		node.health_changed.connect(health_bar._set_health)
 		health_bar.init_health(node.hp)
@@ -56,10 +48,14 @@ func _update_timer_ui() -> void:
 	var seconds = int(time_elapsed) % 60
 	
 	time_label.text = "%02d:%02d" % [minutes, seconds]
-	
+
 func spawn_enemy() -> void:
 	var enemy_node: PackedScene = load("res://scenes/enemies/" + "rock" + ".tscn")
 	var new_enemy: Enemy = enemy_node.instantiate()
 	new_enemy.global_position = Vector2(randf(), randf()) * 100
 	add_child(new_enemy)
-	
+
+func start_game():
+	print("Game Start!")
+	for player_id in Lobby.players:
+		_on_player_connected(player_id) 
