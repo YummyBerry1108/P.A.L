6ff17f7bc88bf848a16508bc65286304bb269908
Anemois <danieltang1668@gmail.com>
2026-03-15 22:59:34 +0800

    new ver for scaled pu and mirrored placing

--- scripts/tiles/tile_map_manager.gd
+++ scripts/tiles/tile_map_manager.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
 @onready var snow: TileMapLayer = $Snow
+@onready var lava: TileMapLayer = $Lava
 
 var MAP_SIZE: Vector2 = Vector2(100, 100)
 
 func _ready() -> void:
-	var four_rotations = [
+	var H_flip = [
 		0,
-		TileSetAtlasSource.TRANSFORM_TRANSPOSE | TileSetAtlasSource.TRANSFORM_FLIP_H,
-		TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_FLIP_V,
-		TileSetAtlasSource.TRANSFORM_TRANSPOSE | TileSetAtlasSource.TRANSFORM_FLIP_V
+		TileSetAtlasSource.TRANSFORM_FLIP_H,
 	]
-	
+	var V_flip = [
+		0,
+		TileSetAtlasSource.TRANSFORM_FLIP_V,
+	]
 	for y in range(-MAP_SIZE.y/2, MAP_SIZE.y/2):
 		for x in range(-MAP_SIZE.x/2, MAP_SIZE.x/2):
-			snow.set_cell(Vector2(x, y), 8, Vector2(0, 0), four_rotations[randi_range(0, 0)])
+			lava.set_cell(Vector2(x, y), 3, Vector2(0, 0), V_flip[((y%2)+2)%2] | H_flip[((x%2)+2)%2])
